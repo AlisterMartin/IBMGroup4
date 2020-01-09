33278efe716ba9dd2477b220f00ddf65518087e3
Alicee5cha <48280073+Alicee5cha@users.noreply.github.com>
2020-01-09 13:14:24 +0000

    Username if/else

--- app/src/main/java/com/example/firstapp/LoginScreen.java
+++ app/src/main/java/com/example/firstapp/LoginScreen.java
@@ -3,6 +3,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -14,36 +15,19 @@ public class LoginScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_screen);
-<<<<<<< HEAD
 
         final Button loginButton = (Button) findViewById(R.id.loginButton);
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Scanner sc = new Scanner(System.in);
-                String userName =
-
-                        EditText editText1 = findViewById(R.id.firstEdit);
-                EditText editText2 = findViewById(R.id.secondEdit);
-                TextView textView1 = findViewById(R.id.Product);
-                int num1 = Integer.parseInt(editText1.getText().toString());
-                int num2 = Integer.parseInt(editText2.getText().toString());
-                int result = num1 * num2;
-                textView1.setText(result + " ");
-
-                startActivity(new Intent(LoginScreen.this, Welcome.class));
-=======
-        Button loginButton = (Button) findViewById(R.id.loginButton);
-        loginButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                TextView username = findViewById(R.id.usernameInput);
+                EditText username = findViewById(R.id.usernameInput);
                 if(username.getText().toString().equalsIgnoreCase("a")){
                     System.out.println("Correct username");
                 }else{
                     System.out.println("Incorrect username");
                 }
->>>>>>> 02917a019525b9b7dbdf6d9c373b128eae59139b
+
+                startActivity(new Intent(LoginScreen.this, Welcome.class));
             }
         });
 

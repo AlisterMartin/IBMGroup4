eb9e4233e868b40f161fe30c4a1bb5721a337812
AlisterMartin <b8023382@my.shu.ac.uk>
2020-01-16 10:33:01 +0000

    Merge

--- example/src/main/java/com/ibm/watson/developer_cloud/android/myapplication/Itinerary.java
+++ example/src/main/java/com/ibm/watson/developer_cloud/android/myapplication/Itinerary.java
@@ -9,7 +9,7 @@ public class Itinerary extends AppCompatActivity {
     @Override
     public void onCreate(Bundle savedInstance){
         super.onCreate(savedInstance);
-        setContentView(R.layout.itinerary);
+        setContentView(R.layout.select_itineray);
 
     }
 }
